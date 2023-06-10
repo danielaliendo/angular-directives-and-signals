b8d1998392190d6f1966c9a6ef93b7439b9e2d0c
Daniela Liendo <daniela@ualabee.com>
2023-06-10 22:36:13 +0200

    signals layout component was created

---
+++ src/app/signals/layout/signals-layout/signals-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'signals-signals-layout',
+  templateUrl: './signals-layout.component.html',
+  styleUrls: ['./signals-layout.component.css']
+})
+
+export class SignalsLayoutComponent {
+
+}
